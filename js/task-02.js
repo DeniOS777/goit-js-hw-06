3ab9fdc0d6a3380dd3ee50ffbbb8c9e7b9a22387
Denis Moskalenko <87602096+DeniOS777@users.noreply.github.com>
2021-12-03 00:45:40 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,16 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const listRefs = document.querySelector('#ingredients');
+
+const ingredientsArray = [];
+
+for (const ingredient of ingredients) {
+  const itemEl = document.createElement('li');
+  itemEl.textContent = `${ingredient}`;
+  itemEl.classList.add('item');
+  ingredientsArray.push(itemEl);
+}
+
+listRefs.append(...ingredientsArray);
+
+console.log(listRefs);
