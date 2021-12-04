b32bb024c8e13431066123e720aec9ca617528b5
Denis Moskalenko <87602096+DeniOS777@users.noreply.github.com>
2021-12-04 23:47:41 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const formRefs = document.querySelector('.login-form');
+console.log(formRefs);
 
+const isLogin = event => {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === '' || password.value === '') {
+    return alert('Заполните поля Email и Password');
+  }
+
+  console.log(`Ваш логин: ${email.value} и пароль: ${password.value}`);
+
+  formRefs.reset();
+};
+
+formRefs.addEventListener('submit', isLogin);
