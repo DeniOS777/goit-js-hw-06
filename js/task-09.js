9649af2ad609634423cd7a83b8a35e761d3c03b3
Denis Moskalenko <87602096+DeniOS777@users.noreply.github.com>
2021-12-05 20:36:06 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -2,13 +2,13 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-const buttonChangeColor = document.querySelector('button');
+const buttonChangeColorRef = document.querySelector('button');
 
-const colorTextValue = document.querySelector('.color');
+const colorTextValueRef = document.querySelector('.color');
 
 const isClickChangeColor = () => {
   document.body.style.backgroundColor = getRandomHexColor();
-  colorTextValue.textContent = `${getRandomHexColor()}`;
+  colorTextValueRef.textContent = `${getRandomHexColor()}`;
 };
 
-buttonChangeColor.addEventListener('click', isClickChangeColor);
+buttonChangeColorRef.addEventListener('click', isClickChangeColor);
