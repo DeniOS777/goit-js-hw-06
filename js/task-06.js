479fdae4f00d8af284e2f415ac32b105e36986be
Denis Moskalenko <87602096+DeniOS777@users.noreply.github.com>
2021-12-05 20:33:11 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,17 +1,17 @@
-const inputSymbolsRefs = document.querySelector('input');
+const inputSymbolsRef = document.querySelector('input');
 
 const isRemoveFocus = event => {
   const numberOfSymbols = event.currentTarget.value.trim().length;
   const minDataLength = event.currentTarget.dataset.length;
 
   if (numberOfSymbols < minDataLength) {
-    inputSymbolsRefs.classList.add('invalid');
-    inputSymbolsRefs.classList.remove('valid');
+    inputSymbolsRef.classList.add('invalid');
+    inputSymbolsRef.classList.remove('valid');
     alert(`Введите значение не меньше: ${minDataLength} символов`);
   } else {
-    inputSymbolsRefs.classList.add('valid');
-    inputSymbolsRefs.classList.remove('invalid');
+    inputSymbolsRef.classList.add('valid');
+    inputSymbolsRef.classList.remove('invalid');
   }
 };
 
-inputSymbolsRefs.addEventListener('blur', isRemoveFocus);
+inputSymbolsRef.addEventListener('blur', isRemoveFocus);
