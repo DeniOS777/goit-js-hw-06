9c11c245cb9decd79e6fb538cdf2a0bd63929f0c
Denis Moskalenko <87602096+DeniOS777@users.noreply.github.com>
2021-12-06 09:45:14 +0200

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,10 +1,9 @@
 const inputRangeValueRef = document.querySelector('#font-size-control');
-
 const spanTextSizeRef = document.querySelector('#text');
 
-const setSizeText = event => {
+const onChangeFontSizeText = event => {
   const variableInputRange = event.currentTarget.value;
   spanTextSizeRef.style.fontSize = `${variableInputRange}px`;
 };
 
-inputRangeValueRef.addEventListener('input', setSizeText);
+inputRangeValueRef.addEventListener('input', onChangeFontSizeText);
