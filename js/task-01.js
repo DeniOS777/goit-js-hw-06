5068ac00e8c3d12fb65ac36762094870d962f00b
Denis Moskalenko <87602096+DeniOS777@users.noreply.github.com>
2021-12-07 18:56:22 +0200

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -7,16 +7,8 @@ console.log(`Number of categories: ${categoriesNumber}`);
 
 const loggingCategory = document.querySelectorAll('h2');
 const amountElements = document.querySelectorAll('.item ul');
-console.log(`Category: ${loggingCategory[0].textContent}`);
-console.log(`Elements: ${amountElements[0].children.length}`);
 
-console.log(`Category: ${loggingCategory[1].textContent}`);
-console.log(`Elements: ${amountElements[1].children.length}`);
-
-console.log(`Category: ${loggingCategory[2].textContent}`);
-console.log(`Elements: ${amountElements[2].children.length}`);
-
-// for (let i = 0; i < loggingCategory.length; i += 1) {
-//   console.log(`Category: ${loggingCategory[i].textContent}`);
-//   console.log(`Elements: ${amountElements[i].children.length}`);
-// }
+for (let i = 0; i < loggingCategory.length; i += 1) {
+  console.log(`Category: ${loggingCategory[i].textContent}`);
+  console.log(`Elements: ${amountElements[i].children.length}`);
+}
