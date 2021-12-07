e75d25b905e6c56aedf28b1df77868599800451e
Denis Moskalenko <87602096+DeniOS777@users.noreply.github.com>
2021-12-07 18:56:26 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -4,10 +4,10 @@ const onRemoveFocus = event => {
   const numberOfSymbols = event.currentTarget.value.trim().length;
   const minDataLength = event.currentTarget.dataset.length;
 
-  if (numberOfSymbols < minDataLength) {
+  if (numberOfSymbols != minDataLength) {
     inputSymbolsRef.classList.add('invalid');
     inputSymbolsRef.classList.remove('valid');
-    return alert(`Введите значение не меньше: ${minDataLength} символов`);
+    return alert(`Введите значение равное: ${minDataLength} символам`);
   }
   inputSymbolsRef.classList.add('valid');
   inputSymbolsRef.classList.remove('invalid');
